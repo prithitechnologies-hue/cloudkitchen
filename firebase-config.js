ecef0d69fbb1fd0bd93b72b9df3e5620748f0ef8
Thirumal Govindaraju <trgthirumal@gmail.com>
2026-09-20 16:18:11 +0530

    Home kitchen order site

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+/* Paste your Firebase web-app config here (Firebase console > Project settings > Your apps).
+   While apiKey still starts with "YOUR", the site uses CONFIG.menu from index.html instead. */
+window.FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID"
+};
